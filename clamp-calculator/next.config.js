ad313312792719084d127c5de1030b42fddbaedc
Vittorio Retrivi <vittorioretrivi@gmail.com>
2022-05-29 23:58:41 -0300

    Add PWA to Clamp Calculator

--- clamp-calculator/next.config.js
+++ clamp-calculator/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(
+  {
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
+  },
+  nextConfig
+);
